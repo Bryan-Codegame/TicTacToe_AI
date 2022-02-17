5392a1148e71be84892b9b56a3f8c625b8653a50
bryanLaptop <bryanesc26@gmail.com>
2022-02-17 00:30:57 -0600

    Define Global constants

---
+++ Tic_Tac_Toe_AI/Main.cpp
@@ -0,0 +1,24 @@
+//Tic-Tac-Toe
+//Playing Tic Tac Toe with an AI
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+//Global Constants
+const char X = 'X';
+const char O = 'O';
+const char T = 'T';
+const char EMPTY = ' ';
+const char NO_ONE = 'N';
+
+//Prototype Functions
+
+
+using namespace std;
+
+//Function main
+int main() {
+
+}
+
